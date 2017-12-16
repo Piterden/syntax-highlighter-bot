c2c27319ddba1b888c137ed525e29431119b837b
Devel <dev812@158015.simplecloud.ru>
2017-12-16 08:53:07 +0000

    Updated migrations

--- migrations/20171126121351_create_uses_table.js
+++ migrations/20171126121351_create_uses_table.js
@@ -2,6 +2,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable('uses', function (table) {
     table.increments()
+    table.string('hash', 32).notNullable()
     table.integer('user_id').unsigned().notNullable()
     table.integer('chat_id').unsigned().notNullable()
     table.string('lang', 40).nullable()
@@ -9,6 +10,7 @@ exports.up = function (knex) {
     table.binary('image')
     table.timestamp('created_at')
 
+    table.index('hash')
     table.index(['user_id', 'chat_id'])
   })
 }
